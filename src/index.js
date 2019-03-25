2df6aad588aae98152530a9b88b4d4936ea0f46f
ryp-app01 <ryp-app01@ryp-app01deiMac.lan>
2019-03-25 14:43:09 +0800

    图片

---
+++ src/index.js
@@ -0,0 +1,17 @@
+export default {
+
+    tabBarItem: {
+            home_Normal: () => { return require('./TabBar/home_Normal.png') },
+            home_Selected: () => { return require('./TabBar/home_Selected.png') },
+            category_Normal: () => { return require('./TabBar/category_Normal.png') },
+            category_Selected: () => { return require('./TabBar/category_Selected.png') },
+            saleSell_Normal: () => { return require('./TabBar/saleSell_Normal.png') },
+            saleSell_Selected: () => { return require('./TabBar/saleSell_Selected.png') },
+            car_Normal: () => { return require('./TabBar/car_Normal.png') },
+            car_Selected: () => { return require('./TabBar/car_Selected.png') },
+            me_Normal: () => { return require('./TabBar/me_Normal.png') },
+            me_Selected: () => { return require('./TabBar/me_Selected.png') },
+    },
+
+
+}
