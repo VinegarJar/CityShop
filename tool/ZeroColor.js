ea915061d59e0a81e761f75251aa4dca2392959a
tock <2966497308@qq.com>
2020-02-06 22:50:20 +0800

    删除

--- tool/ZeroColor.js
+++
@@ -1,6 +0,0 @@
-//#06C1AE #e0e0e0 #f3f3f3
-export default {
-    theme: '#EB5148',
-    border: '#e0e0e0',
-    background: '#f3f3f3'
-}
--- tool/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducers from '../reducers/index';
-const initialState = {};
-const middleware = [thunk];
-export const store = createStore( rootReducers,initialState, applyMiddleware(...middleware));
