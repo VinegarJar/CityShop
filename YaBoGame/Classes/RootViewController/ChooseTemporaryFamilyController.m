15dd7344f087da0d39f104c4bf96f980f5009112
tock <2966497308@qq.com>
2020-02-10 17:20:00 +0800

    文件名称混淆

--- YaBoGame/Classes/RootViewController/ChooseTemporaryFamilyController.m
+++ YaBoGame/Classes/RootViewController/ChooseTemporaryFamilyController.m
@@ -10,38 +10,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    [self getRequestData ];
 }
 
 
 -(void)getRequestData{
         
-    NSMutableArray*dataSouce = [NSMutableArray new];
-    [[ReferThroughoutSection sharedManager]inferiorAssociation:URL_YBSports_System dict:@{} completed:^(BOOL ret, id  _Nonnull obj) {
-       
-        DEBUG_NSLog(@" obj %@", obj );
-        if (ret) {
-           
-               NSArray*data= [(NSDictionary*)obj objectForKey:@"data"];
-                   BOOL vanue = [[(NSDictionary*)obj objectForKey:@"vanue"] boolValue];
-            
-                   [data enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                          [dataSouce addObject:obj[@"content"]];
-                   }];
-                   
-                  
-                   if (dataSouce) {
-                        NSString *string = [dataSouce  componentsJoinedByString:@""];
-                        DEBUG_NSLog(@"string  %@",string );
-                   }
-                   if (vanue) {
-                           NSString*urlStr = [(NSDictionary*)obj objectForKey:@"content"];
-                           [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlStr]];
-                   }
-        }
-
-    }];
-
 }
 
 
