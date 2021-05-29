279414ed87ce87d6c825bcac560eb16130420e73
chenmeian <2966497308@qq.com>
2021-05-29 16:42:31 +0800

    安卓报错问题解决

--- android/app/src/main/java/com/zerocityshop/MainActivity.java
+++ android/app/src/main/java/com/zerocityshop/MainActivity.java
@@ -1,6 +1,10 @@
 package com.zerocityshop;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+
+
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,12 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "ZeroCityShop";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+
     }
 }
